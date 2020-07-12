b35be94a4457aaef54e8996d91977bb26e05b13a
PABCon <adregaconsulting@gmail.com>
2020-07-12 13:27:05 +0200

    NAV BAR added to my code

---
+++ Odin_Project/randomCode/NAV BAR/script.js
@@ -0,0 +1,29 @@
+const navSlide = () => {
+
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav_link");
+    const navLinks = document.querySelectorAll('.nav_link li');
+
+    burger.addEventListener('click', () => {
+
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else { link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.75}s`; }
+
+        });
+
+        //burguer animation
+
+        burger.classList.toggle('toggle');
+
+    });
+
+}
+
+
+navSlide();
